2ff71f25eecae4e310825a0aa4644f1b343cc166
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-01 22:14:53 -0600

    added a new candidate, added a post api call to server file

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const sqlite3 = require('sqlite3').verbose();
 const { response } = require('express');
 const express = require('express');
 
+const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -70,10 +72,32 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
   });
 
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//             VALUES(?,?,?,?)`;
 
-
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+  
+    const sql =  `INSERT INTO candidates (first_name, last_name, industry_connected) 
+                  VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use this
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
+      });
+    });
+  });
 
 
 // Default response for any other request(Not Found) Catch all
