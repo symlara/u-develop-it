d4081b6c1f0ea0b9aef8649bc749071af5d816c5
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-01 21:58:59 -0600

    removed a candidate. added get and database calls to server file

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const sqlite3 = require('sqlite3').verbose();
+const { response } = require('express');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,9 +8,70 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.log(err.message);
+    }
+
+    console.log('Connected to the election database.');
+});
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
 
 
+  // Get single candidate
+  app.get('/api/candidate/:id', (req,res) => { 
+      const sql = `SELECT * FROM candidates 
+      WHERE id = ?`;
+      const params = [req.params.id];
+db.get(sql, params, (err, row) => {
+    if(err) {
+         res.status(400).json({ error: err.message });
+         return;
+    }
+    res.json({
+        message: 'success',
+        data: row
+         });
+    });
+});
 
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({
+        message: 'successfully deleted',
+        changes: this.changes
+      });
+    });
+  });
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES(?,?,?,?)`;
 
 
 
@@ -19,11 +82,9 @@ app.use((req, res) => {
   });
 
 
-
-
-
-
-
+// Start server after DB connection
+db.on('open', () => { 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    });
 });
